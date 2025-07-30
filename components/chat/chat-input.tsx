a009f4ec919f879d5fe7601f910be87e1b271e2e
Suyog241005 <166608895+Suyog241005@users.noreply.github.com>
2025-07-31 02:55:06 +0530

    emoji removed

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -9,7 +9,6 @@ import qs from "query-string";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useModal } from "@/hooks/use-modal-store";
-import { EmojiPicker } from "@/components/emoji-picker";
 import { useRouter } from "next/navigation";
 
 interface ChatInputProps {
@@ -77,11 +76,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                     }`}
                     {...field}
                   />
-                  <div className="absolute top-7 right-8">
-                    <EmojiPicker 
-                      onChange={(emoji:string)=>field.onChange(`${field.value}${emoji}`)}
-                    />
-                  </div>
+                 
                 </div>
               </FormControl>
             </FormItem>
